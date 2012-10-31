6a29574f3f9cc4fbf4e07567757994e9f84d6435
Mike <michael.macdermaid@tap.me>
2012-10-31 15:16:50 -0500

    Added nil checks for [TBXML elementName:], [TBXML attributeName:], [TBXML attributeValue:], and [TBXML textForElement:], if you pass in nil you will receive an EXC_BAD_ACCESS instead of returning emptystring

--- TBXML-Code/TBXML.m
+++ TBXML-Code/TBXML.m
@@ -256,7 +256,7 @@
 @implementation TBXML (StaticFunctions)
 
 + (NSString*) elementName:(TBXMLElement*)aXMLElement {
-	if (nil == aXMLElement->name) return @"";
+	if (nil == aXMLElement || nil == aXMLElement->name) return @"";
 	return [NSString stringWithCString:&aXMLElement->name[0] encoding:NSUTF8StringEncoding];
 }
 
@@ -277,7 +277,7 @@
 }
 
 + (NSString*) attributeName:(TBXMLAttribute*)aXMLAttribute {
-	if (nil == aXMLAttribute->name) return @"";
+	if (nil == aXMLAttribute || nil == aXMLAttribute->name) return @"";
 	return [NSString stringWithCString:&aXMLAttribute->name[0] encoding:NSUTF8StringEncoding];
 }
 
@@ -299,7 +299,7 @@
 
 
 + (NSString*) attributeValue:(TBXMLAttribute*)aXMLAttribute {
-	if (nil == aXMLAttribute->value) return @"";
+	if (nil == aXMLAttribute || nil == aXMLAttribute->value) return @"";
 	return [NSString stringWithCString:&aXMLAttribute->value[0] encoding:NSUTF8StringEncoding];
 }
 
@@ -314,7 +314,7 @@
 }
 
 + (NSString*) textForElement:(TBXMLElement*)aXMLElement {
-	if (nil == aXMLElement->text) return @"";
+	if (nil == aXMLElement || nil == aXMLElement->text) return @"";
 	return [NSString stringWithCString:&aXMLElement->text[0] encoding:NSUTF8StringEncoding];
 }
 
